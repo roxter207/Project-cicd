5ab3ce4c4a1c570cb868582215083847e1f22bb2
roxter207 <66411952+roxter207@users.noreply.github.com>
2025-08-04 20:37:59 -0400

    converted something

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const app = require('../index');
+import request from 'supertest';
+import app from '../index';
 
 describe('GET /', () => {
   it('should return 200 OK', async () => {
